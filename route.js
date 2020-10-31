7fa65d9670cde039f966df8808ae886d6885fe50
Jonathan Darwin <jonathanjojo38@yahoo.com>
2020-10-31 10:36:57 +0700

    initial commit

---
+++ route.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/indonesia', (req, res) => {
+    res.send({
+        status : 1,
+        message : 'OK',
+        data : {
+            death : 123,
+            recovered : 456,
+            positive : 789,
+            lastUpdated : '2020-10-31 10:35:00'
+        }        
+    })
+})
+
+app.listen(8888, () => {
+    console.log(`Listen to port : 8888`)
+})
